b138e5d7f142f0532fb34fc0fb4259a1d46ee7ee
confytech <confiehiemere@gmail.com>
2025-12-13 23:42:46 +0100

    Add /api/ route and API app

--- studyhub_backend_config/urls.py
+++ studyhub_backend_config/urls.py
@@ -5,6 +5,7 @@ from apps.study.views import home  # Import the homepage view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),  # Homepage route
+    path('api/', include('apps.api.urls')),  # Added this line for the api app
     path('api/users/', include('apps.users.urls')),
     path('api/study/', include('apps.study.urls')),
     path('api/payments/', include('apps.payments.urls')),
